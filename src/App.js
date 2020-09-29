868a379e4dba38f56ec30bb008752b3c5538e833
Jude Okagu <juud0010@gmail.com>
2020-09-29 19:39:55 +0200

    ff

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './App.css';
+
+const App = () => {
+  return (
+    <div className='App'>
+      <h3>we work here and all starts at app.js now </h3>
+    </div>
+  );
+};
+
+export default App;
---
+++ src/algorithms/Queue.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const Queue = () => {
+  return <div></div>;
+};
+
+export default Queue;
